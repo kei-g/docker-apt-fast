aedef778b1f5c8f82b9c97238665ac49b7a47566
kei-g <km.8k6ce+github@gmail.com>
2022-10-07 13:22:06 +0900

    :hammer: Chore build script
    - Prune dangling images before building the image
    - Pull the latest image before building the image
    
    Signed-off-by: kei-g <km.8k6ce+github@gmail.com>

--- build.sh
+++ build.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
+
+CONTAINERS=$(docker ps --all --filter=status=exited --format="{{.ID}}" | xargs)
+[ -z "$CONTAINERS" ] || docker rm "$CONTAINERS"
+
+IMAGES=$(docker images --filter=dangling=true --format="{{.ID}}" | xargs)
+[ -z "$IMAGES" ] || docker rmi "$IMAGES"
+
+docker pull debian:stable-slim
+
 docker build -t snowstep/apt-fast .
